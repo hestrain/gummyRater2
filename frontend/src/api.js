f5b4044a0fe7544cf8c6068fbe00ebcd78c65b08
Heather Strain <64221009+hestrain@users.noreply.github.com>
2025-03-06 21:43:31 -0500

    well at least its working locally again lmao

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,39 +1,40 @@
 import axios from "axios";
+// const router = require('express').Router()
 
 
-const URL = "http://localhost:3001"
+let URL =  "http://localhost:4000";
 
 
-export async function getUsers() {
-    const response = await axios.get(`${URL}/users`)
-    if (response.status === 200) {
-        return response.data
-    } else {
-        return
-    }
-}
-export async function getUser(id) {
-    const response = await axios.get(`${URL}/users/${id}`)
-    if (response.status === 200) {
-        return response.data
-    } else {
-        return
-    }
-}
-export async function createUser(user) {
-    const response = await axios.post(`${URL}/users`, user)
-        return response
-}
-export async function updateUser(id, user) {
-    const response = await axios.put(`${URL}/users/${id}`, user)
-        return response
-}
-export async function deleteUser(id) {
-    const response = await axios.delete(`${URL}/users/${id}`)
-        return response
-}
+// export async function getUsers() {
+//     const response = await axios.get(`${URL}/users`)
+//     if (response.status === 200) {
+//         return response.data
+//     } else {
+//         return
+//     }
+// }
+// export async function getUser(id) {
+//     const response = await axios.get(`${URL}/users/${id}`)
+//     if (response.status === 200) {
+//         return response.data
+//     } else {
+//         return
+//     }
+// }
+// export async function createUser(user) {
+//     const response = await axios.post(`${URL}/users`, user)
+//         return response
+// }
+// export async function updateUser(id, user) {
+//     const response = await axios.put(`${URL}/users/${id}`, user)
+//         return response
+// }
+// export async function deleteUser(id) {
+//     const response = await axios.delete(`${URL}/users/${id}`)
+//         return response
+// }
 export async function getGummys() {
-    console.log("getting all gummys");
+    // console.log("getting all gummys");
     
     const response = await axios.get(`${URL}/gummy`)
     if (response.status === 200) {
@@ -67,3 +68,28 @@ export async function deleteGummy(id) {
     const response = await axios.delete(`${URL}/gummy/${id}`)
         return response
 }
+
+
+// export const getGummys = () => {
+//     return fetch('/api/gummys'), {
+//         method: 'GET',
+//         headers:{
+//             'Content-Type':'application/json',
+//         },
+//     }
+// }
+// export const createGummy = (gummy) => {
+//     return fetch('/api/gummys'), {
+//         method: 'POST',
+//         headers:{
+//             'Content-Type':'application/json',
+            
+//         },
+//         body: JSON.stringify(gummy)
+//     }
+// }
+// export const deleteGummy = (id) => {
+//     return fetch(`${URL}`api/gummy/${id}`), {
+//         method: 'DELETE',
+//     }
+// }

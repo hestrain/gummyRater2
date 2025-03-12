8e7f086535e1d54d073c509d005a84938b1f3f4c
Heather Strain <64221009+hestrain@users.noreply.github.com>
2025-03-12 18:00:42 -0400

    another connection test

--- server/index.js
+++ server/index.js
@@ -2,16 +2,22 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const UserModel = require('./models/Users')
+const GummyModel = require('./models/gummy.model')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017")
+mongoose.connect("mongodb://0.0.0.0:0")
 
 app.get('/getUsers', (req,res) => {
     UserModel.find()
     .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+app.get('/api/gummy', (req,res) => {
+    GummyModel.find()
+    .then(users => res.json(gummy))
     .catch(err => res.json(err))
 })
 
